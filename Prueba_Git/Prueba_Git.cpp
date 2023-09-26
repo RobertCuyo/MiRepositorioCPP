97ae849561ce29467d21bd21dd81679c5b130bb0
Robert <rob_charl@outlook.com>
2023-09-26 12:30:08 -0500

    Separacion de palabras

--- Prueba_Git/Prueba_Git.cpp
+++ Prueba_Git/Prueba_Git.cpp
@@ -1,68 +1,87 @@
 #include <iostream>
+#include <string>
+#include <sstream> 
 using namespace std;
+
 template<class T>
 class Lista;
-/*===========================================================*/
+
 template <class T>
 class Nodo {
 private:
-	Nodo<T>* izq;
-	Nodo<T>* der;
-	T info;
+    Nodo<T>* izq;
+    Nodo<T>* der;
+    T info;
 public:
-	Nodo();
-	friend class Lista<T>;
-
+    Nodo();
+    friend class Lista<T>;
 };
+
 template <class T>
 Nodo<T>::Nodo() : izq(nullptr), der(nullptr), info(T()) {
-	izq = NULL;
-	der = NULL;
+    izq = NULL;
+    der = NULL;
 }
-/*===========================================================*/
+
 template <class T>
 class Lista {
 private:
-	Nodo<T>* primero;
-	Nodo<T>* ultimo;
+    Nodo<T>* primero;
+    Nodo<T>* ultimo;
 public:
-	Lista();
-	void insertaFinal(T);
-	void imprimir();
-	T regUlti() {
-		return ultimo;
-	}
+    Lista();
+    void insertaFinal(T);
+    void imprimir();
+    Nodo<T>* obtenerPrimero() {
+        return primero;
+    }
 };
+
 template<class T>
 Lista<T>::Lista() {
-	primero = NULL;
-	ultimo = NULL;
+    primero = NULL;
+    ultimo = NULL;
 }
+
 template<class T>
 void Lista<T>::insertaFinal(T dato) {
-	Nodo<T>* apunt = new Nodo<T>;
-	apunt->info = dato;
-	apunt->izq = ultimo;
-	if (ultimo) {
-		ultimo->der = apunt;
-	}
-	else {
-		primero = apunt;
-	}
-	ultimo = apunt;
+    Nodo<T>* apunt = new Nodo<T>;
+    apunt->info = dato;
+    apunt->izq = ultimo;
+    if (ultimo) {
+        ultimo->der = apunt;
+    }
+    else {
+        primero = apunt;
+    }
+    ultimo = apunt;
 }
+
 template<class T>
 void Lista<T>::imprimir() {
-	Nodo<T>* apunt = primero;
-	while (apunt) {
-		cout << apunt->info << '\n';
-		apunt = apunt->der;
-	}
+    Nodo<T>* apunt = primero;
+    while (apunt) {
+        cout << apunt->info << '\n';
+        apunt = apunt->der;
+    }
+    cout << endl;
 }
+
 int main() {
-	Lista<string> cadena;
+    Lista<string> lista;
 
+    cout << "-> ";
+    string oracion;
+    getline(cin, oracion);
 
+    // Dividir la oración en palabras
+    istringstream iss(oracion);
+    string palabra;
+    while (iss >> palabra) {
+        lista.insertaFinal(palabra);
+    }
+    
+    lista.imprimir();
 
-	return 0;
+    return 0;
 }
